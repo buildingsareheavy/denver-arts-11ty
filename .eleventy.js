390a1da5fd32115fb22be5490825e9dd0e98b9ed
Ethan Barrett <hello@buildingsareheavy.com>
2023-05-29 19:36:09 -0600

    added sass support

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("styles.css");
+   
     return {
         dir: {
             input: "src",
